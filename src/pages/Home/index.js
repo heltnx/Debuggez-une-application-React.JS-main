4005c1d609ca93ff6da598a4844b92a2b9fc11f0
heltnx <helenetnx@yahoo.fr>
2023-11-19 15:47:56 +0100

    debug console “src image” et “tittle” required

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -115,14 +115,18 @@ const Page = () => {
     </main>
     <footer className="row">
       <div className="col presta">
-        <h3>Notre derniére prestation</h3>
-        <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
-          date={new Date(last?.date)}
-          small
-          label="boom"
-        />
+        <h3>Notre dernière prestation</h3>
+        {last ? (
+          <EventCard
+            imageSrc={last.cover}
+            title={last.title}
+            date={new Date(last.date)}
+            small
+            label="boom"
+          />
+        ) : (
+          <p>Chargement de la dernière prestation...</p>
+        )}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
